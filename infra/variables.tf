c37e6cbc4b51b58724d4055ba081c17672c270dd
Shekhor <shekhor.chanda333@gmail.com>
2026-05-08 09:15:41 -0500

    Update .gitignore to remove variables.tf and add new variables.tf for Terraform configuration
    - Removed `variables.tf` from .gitignore.
    - Added new `variables.tf` file defining environment, location, project name, and database credentials for Terraform.

---
+++ infra/variables.tf
@@ -0,0 +1,29 @@
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "prod"
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+  default     = "canadacentral"
+}
+
+variable "project_name" {
+  description = "Project name used for naming resources"
+  type        = string
+  default     = "assistiq"
+}
+
+variable "db_admin_username" {
+  description = "Database administrator username"
+  type        = string
+  default     = "sqladmin"
+}
+
+variable "db_admin_password" {
+  description = "Database administrator password"
+  type        = string
+  sensitive   = true
+}
